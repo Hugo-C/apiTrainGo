2a4bc16d5d3dd5c4cce9e63f2151f100e298faa1
Hugo-C <hchastel@laposte.net>
2019-02-14 23:55:39 +0100

    Changed port

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ type responseKmToPrice struct {
 // Return a price in the wanted devise based on the distance
 func KmToPrice(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
-	fmt.Println(params["devise"])  // TODO handle several devise
+	fmt.Println(params["devise"]) // TODO handle several devise
 	distance, err := strconv.ParseFloat(params["km"], 64)
 	if err != nil {
 		fmt.Println("bad parameters : " + params["km"])
@@ -42,9 +42,9 @@ func KmToPrice(w http.ResponseWriter, r *http.Request) {
 
 // main function to boot up everything
 func main() {
-    port := "8000"
+	port := "80"
 	router := mux.NewRouter()
 	fmt.Println("Server running on port " + port)
 	router.HandleFunc("/kmToPrice/{devise}/{km:[0-9]+.[0-9]+}", KmToPrice).Methods("GET")
-	log.Fatal(http.ListenAndServe(":" + port, router))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
